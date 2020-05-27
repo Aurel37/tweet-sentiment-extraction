82c714b567ee4dc360537275cf672ae66914cc17
Aurel37 <pion.aurelien@outlook.com>
2020-05-27 20:13:46 +0200

    ajout de fonctions utiles, chargement des donnees et de la metrique

---
+++ utils/data_loader.py
@@ -0,0 +1,14 @@
+"""open the csv document and convert them into numpy array"""
+from __future__ import absolute_import
+import pandas as pd
+
+
+def open_csv(path, *col):
+    """return the data converted into a numpy array in a set
+    path : path of the csv document
+    col : columns that may be interesting"""
+    data = pd.read_csv(path)
+    res = []
+    for i in col:
+        res.append(data[i].to_numpy())
+    return res
---
+++ utils/metric.py
@@ -0,0 +1,9 @@
+"""metric definition"""
+from __future__ import absolute_import
+
+
+def jaccard(str1, str2):
+    a = set(str1.lower().split())
+    b = set(str2.lower().split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
