94ac5a9e1be86e123a899e64334365bd74408e9a
Sébastien Morel <sebastien.morel@eleves.enpc.fr>
2020-05-28 22:29:39 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from utils.metric import jaccard
 from utils.text_prep import vectorize
 from utils.data_loader import open_csv
@@ -7,8 +8,47 @@ train = open_csv('train.csv', 'text', 'selected_text', 'sentiment')
 
 #vectorize the text
 # /!\ the sets text and selected_text are not vectorized on the same word base yet
-text_train = vectorize(train[0])
+text_train, translation = vectorize(train[0])
 
-histo = np.zeros(text_train.shape)
-for l in text_train:
-    histo += l
+def histo_repartition(text, words, N):
+    positive = np.zeros(text.shape[1])
+    negative = np.zeros(text.shape[1])
+    neutral = np.zeros(text.shape[1])
+    for l in range(len(text)):
+        if train[-1][l] == 0:
+            neutral += text[l]
+        elif train[-1][l] == 1:
+            positive += text[l]
+        else:
+            negative += text[l]
+    positive_index = np.argsort(positive)
+    positive_index = np.flipud(positive_index) 
+    values = [positive[positive_index[i]] for i in range(N)]
+    popular_words = [words[positive_index[i]] for i in range(N)]
+    x = np.arange(N)
+    plt.xticks(x, popular_words)
+    plt.plot(x, values)
+    plt.title("Positive")
+    plt.show()
+                     
+    negative_index = np.argsort(negative)
+    negative_index = np.flipud(negative_index) 
+    values = [negative[negative_index[i]] for i in range(N)]
+    popular_words = [words[negative_index[i]] for i in range(N)]
+    plt.xticks(x, popular_words )
+    plt.plot(x, values)
+    plt.title("Negative")
+    plt.show()
+
+                     
+    neutral_index = np.argsort(neutral)
+    neutral_index = np.flipud(neutral_index) 
+    values = [neutral[neutral_index[i]] for i in range(N)]
+    popular_words = [words[neutral_index[i]] for i in range(N)]
+    plt.xticks(x, popular_words )
+    plt.plot(x, values)
+    plt.title("Neutral")
+    plt.show()
+
+N = 10
+histo_repartition(text_train, translation, 20)
