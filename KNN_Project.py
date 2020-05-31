5385d8eb5433fb0cb81609e48e7d067e30bb0bf0
JLandais <j.landais3@gmail.com>
2020-05-31 15:36:06 +0200

    kNN v2 and treatment in main

--- KNN_Project.py
+++ KNN_Project.py
@@ -9,6 +9,7 @@ Created on Sat May 30 21:17:21 2020
 import numpy as np
 import scipy.io as sio
 import csv
+from utils.data_loader import open_csv
 from utils.text_prep import vectorize, clean
 
 
@@ -34,8 +35,11 @@ positive_tweets = np.array(tweets_1)
 negative_tweets = np.array(tweets_2)
 
 tweets_classes = np.array((neutral_tweets, positive_tweets, negative_tweets))
+n = len(tweets_classes[0]) + len(tweets_classes[1]) + len(tweets_classes[2])
 
 def jaccard(str1, str2): 
+    if str1 == str2:
+        return float(n)
     a = set(str1.lower().split()) 
     b = set(str2.lower().split())
     c = a.intersection(b)
@@ -56,64 +60,65 @@ def jaccard_lettres(str1, str2):
     # print(c)
     return float(len(c)) / (len(a) + len(b) - len(c))
 
-def separe_mots(phrase):
-    liste = phrase.split()
-    return liste
-
 def KNN_word(chaine):
     distances = np.array([0., 0., 0.])
     for phrase in neutral_tweets:
-        mots = separe_mots(phrase)
+        mots = phrase.split()
         for word in mots:
             distances[0] += jaccard_lettres(chaine, word)
     distances[0] = distances[0] / neutral_tweets.shape[0]
     for phrase in positive_tweets:
-        mots = separe_mots(phrase)
+        mots = phrase.split()
         for word in mots:
             distances[1] += jaccard_lettres(chaine, word)
     distances[1] = distances[1] / positive_tweets.shape[0]
     for phrase in negative_tweets:
-        mots = separe_mots(phrase)
+        mots = phrase.split()
         for word in mots:
             distances[2] += jaccard_lettres(chaine, word)
     distances[2] = distances[2] / negative_tweets.shape[0]
     return np.argmax(distances)
 
 def KNN_sentence(chaine):
+    chaine = str(chaine)
     similarites = np.array([0., 0., 0.])
     for tweet in neutral_tweets:
-        similarites[0] += jaccard(tweet, chaine)
+        similarites[0] += jaccard(str(tweet), chaine)
     similarites[0] = similarites[0] / neutral_tweets.shape[0]
     for tweet in positive_tweets:
-        similarites[1] += jaccard(tweet, chaine)
+        similarites[1] += jaccard(str(tweet), chaine)
     similarites[1] = similarites[1] / positive_tweets.shape[0]
     for tweet in negative_tweets:
-        similarites[2] += jaccard(tweet, chaine)
+        similarites[2] += jaccard(str(tweet), chaine)
     similarites[2] = similarites[2] / negative_tweets.shape[0]
     return np.argmax(similarites)
 
 def KNN(chaine):
-    mots = separe_mots(chaine)
+    chaine = clean(chaine)
+    mots = chaine.split()
     classe = 0
-    if len(mots) == 0:
-        return("Erreur, chaine vide")
-    if len(mots) == 1:
+    if len(mots) <= 1:
         classe = KNN_word(chaine)
     else:
         classe = KNN_sentence(chaine)
     return(classe)
-    
+
+"""
+Premier test pour identifier la partie du tweet qui génère l'émotion
+
 def identifie_partie_emotion(chaine):
     classe = KNN(chaine)
     if classe == 0:
         return("Tweet neutre, pas d'emotions detectee.")
-    mots = separe_mots(phrase)
+    mots = chaine.split()
     chaine1 = ""
     chaine2 = ""
     for i in range(len(mots)//2):
         chaine1 += mots[i]
     for i in range(len(mots)//2, len(mots)):
-        chaine2 += mots[i]
+        chaine2 += mots[i]+" "
+    print(chaine1)
+    print(chaine2)
     classe1 = KNN(chaine1)
     classe2 = KNN(chaine2)
     if classe1 != 0 and classe2 != 0:
@@ -122,6 +127,70 @@ def identifie_partie_emotion(chaine):
         return chaine1
     else:
         return chaine2
-            
-        
+"""
+  
+def recursive_selection(chaine, original_class):
+    classe = KNN(chaine)
+    if classe != original_class:
+        return ''
+    mots = chaine.split()
+    if len(mots) == 1:
+        return chaine
+    chaine1 = ""
+    chaine2 = ""
+    for i in range(len(mots)//2):
+        chaine1 += mots[i]+" "
+    for i in range(len(mots)//2, len(mots)):
+        chaine2 += mots[i]+" "
+    a = select_emotive_part(chaine1, original_class)
+    b = select_emotive_part(chaine2, original_class)
+    if len(a) >= len(b):
+        return a
+    else:
+        return b
     
+def simple_selection(chaine, original_class):
+    mots = chaine.split()
+    if len(mots) == 1:
+        return chaine
+    chaine1 = ""
+    chaine2 = ""
+    for i in range(len(mots)//2):
+        chaine1 += mots[i]+" "
+    for i in range(len(mots)//2, len(mots)):
+        chaine2 += mots[i]+" "
+    classe1 = KNN(chaine1)
+    classe2 = KNN(chaine2)
+    if classe1 != original_class and classe2 != original_class:
+        return chaine
+    if classe1 == original_class:
+        return chaine1
+    else:
+        return chaine2
+    
+def simple_selection_bis(chaine, original_class):
+    mots = chaine.split()
+    if len(mots) == 1:
+        return chaine
+    chaine1 = ""
+    chaine2 = ""
+    chaine3 = ""
+    for i in range(len(mots)//3):
+        chaine1 += mots[i]+" "
+    for i in range(len(mots)//3, 2*len(mots)//3):
+        chaine2 += mots[i]+" "
+    for i in range(2*len(mots)//3, len(mots)):
+        chaine3 += mots[i]+" "
+    classe1 = KNN(chaine1)
+    classe2 = KNN(chaine2)
+    classe3 = KNN(chaine3)
+    if classe1 != original_class and classe2 != original_class and classe3 != original_class:
+        return chaine
+    if classe1 == original_class:
+        return chaine1
+    elif classe2 == original_class:
+        return chaine2
+    else:
+        return chaine3
+
+
--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import csv
 from utils.text_prep import vectorize, clean
 from utils.data_loader import open_csv, get_x_by_label, get_x_not_by_label
 from utils.manipulation import *
+from KNN_Project import simple_selection, simple_selection_bis, recursive_selection
 
-train = open_csv('train.csv', 'text', 'selected_text', 'sentiment')
 
 #vectorize the text
-
-
 def vectorize_pca(document, column, lb, dimpca):
     text = get_x_not_by_label('train.csv', 'test', 'neutral')
     test, d = vectorize(text)
@@ -21,27 +19,64 @@ def vectorize_pca(document, column, lb, dimpca):
     text_array_pca = PCA(text_array_st, dimpca)
     return text_array_pca
 
+# treats the lists of positive and negatives tweets 
+def treatment(positives, negatives):
+    positives_treated = []
+    negatives_treated = []
+    for i in range(len(positives)):
+        positives_treated.append(simple_selection(positives[i], 1))
+    for i in range(len(negatives)):
+        negatives_treated[i].append(simple_selection(negatives[i], 2))
+    final_list = []
+    final_list.append(positives_treated)
+    final_list.append(negatives_treated)
+    return final_array
+    
+test = open_csv('train.csv', 'text', 'selected_text', 'sentiment')
+tweets = test[0]
+labels = test[2]
+data = np.array((train, labels))
 
-#selected_text = get_x_not_by_label('train.csv', "text", "neutral")
-#test, d = vectorize(selected_text)
-#print(len(d))
+neutral_tweets = [] # neutral tweets
+positive_tweets = [] # positive tweets
+negative_tweets = [] # negative tweets
 
-#pauvres, histo = peu_repeter(test, d, 5)
-#print(histo[0:100])
-#make_histo(histo, "Histogramme des repetitions des mots de notre ensemble", "blue")
-#print(len(pauvres))
+for i in range(len(tweets)):
+    if labels[i]==0:
+        neutral_tweets.append(tweets[i])
+    elif labels[i]==1:
+        positive_tweets.append(tweets[i])
+    else:
+        negative_tweets.append(tweets[i])
+        
+tweets_classes = np.array((neutral_tweets, positive_tweets, negative_tweets))
+n = len(tweets_classes[0]) + len(tweets_classes[1]) + len(tweets_classes[2])
 
-#riches = difference(d, pauvres)
+# Treament of tweets : 
+# treated_list = treatment(positive_tweets, negative_tweets)
+
+"""
+selected_text = get_x_not_by_label('train.csv', "text", "neutral")
+test, d = vectorize(selected_text)
+print(len(d))
+
+pauvres, histo = peu_repeter(test, d, 5)
+print(histo[0:100])
+make_histo(histo, "Histogramme des repetitions des mots de notre ensemble", "blue")
+print(len(pauvres))
+
+riches = difference(d, pauvres)
 
 
-#text_test = np.array(open_csv('test.csv', 'text', 'sentiment'))
-#vect_test, d = vectorize(text_test[0], riches)
-#print("vectorialisation finie")
-#res = build(vect_test, d)
-#print(res[36])
+text_test = np.array(open_csv('test.csv', 'text', 'sentiment'))
+vect_test, d = vectorize(text_test[0], riches)
+print("vectorialisation finie")
+res = build(vect_test, d)
+print(res[36])
 
-#begin = slices(text_test[0])
-#print(begin[36])
-#print("slice")
-#final = arange_string(res, begin)
-#content = ecrire_resultat(text_test[0], final, text_test[-1])
+begin = slices(text_test[0])
+print(begin[36])
+print("slice")
+final = arange_string(res, begin)
+content = ecrire_resultat(text_test[0], final, text_test[-1])
+"""
