217076a8465a1f57a58ceb30c562b9d8fb175b93
Aurel37 <pion.aurelien@outlook.com>
2020-05-31 18:38:56 +0200

    modified:   main.py 	new file:   test.py 	new file:   utils/classification.py 	new file:   utils/display.py 	new file:   utils/reduction.py 	modified:   utils/text_prep.py

--- main.py
+++ main.py
@@ -2,26 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.metric import jaccard
 import csv
-from utils.text_prep import vectorize, clean
-from utils.data_loader import open_csv, get_x_by_label, get_x_not_by_label
-from utils.manipulation import *
-
-train = open_csv('train.csv', 'text', 'selected_text', 'sentiment')
-
-#vectorize the text
+from utils.text_prep import vectorize
+from utils.data_loader import get_x_not_by_label
+from utils.manipulation import peu_repeter, difference
+from utils.classification import log_reg
+from utils.reduction import PCA, standardize
 
 
-def vectorize_pca(document, column, lb, dimpca):
-    text = get_x_not_by_label('train.csv', 'test', 'neutral')
+def vectorize_pca(document, dimpca, x_col, lb_col):
+    text = get_x_not_by_label(document, x_col, 'neutral')
+    selected_text = get_x_not_by_label(document, lb_col, 'neutral')
+
     test, d = vectorize(text)
     pauvres, histo = peu_repeter(test, d, 5)
     riches = difference(d, pauvres)
     text_array_clean, dico = vectorize(text, riches)
+    sel_text_array, dico = vectorize(selected_text, riches)
     text_array_st = standardize(text_array_clean)
     text_array_pca = PCA(text_array_st, dimpca)
-    return text_array_pca
+    return text_array_pca, sel_text_array
 
 
+def classification(document, dimpca, x_col, lb_col):
+    X, Y = vectorize_pca(document, dimpca, x_col, lb_col)
+    n = len(X)
+    classify = log_reg()
+    r = np.random.permutation(n)
+    X_train = X[r[:n]]
+    Y_train = Y[r[:n]]
+    m = len(Y_train[0])
+    res = np.zeros((n, dimpca+1))
+    for i in range(n):
+        classify.fit(X_train, Y_train[:,i])
+        res[i] = classify.beta
+    np.savetxt('test.txt', res)
+    return res
+
+
+classification('train.csv', 25, 'text', 'selected_text')
 #selected_text = get_x_not_by_label('train.csv', "text", "neutral")
 #test, d = vectorize(selected_text)
 #print(len(d))
